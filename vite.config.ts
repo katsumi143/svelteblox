dd0c74fd6697a966c3989a3c6c4a2a955377864c
Blookers <blookyl305@gmail.com>
2023-03-25 22:23:19 +1000

    fix build error

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,8 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import type { UserConfig } from 'vite';
-const config: UserConfig = {
-	server: {
-		
+export default {
+	build: {
+		target: 'esnext'
 	},
 	plugins: [sveltekit()]
-};
-
-export default config;
+} satisfies UserConfig;
