e3597c6abb71c3690f3ef22cef8bb5053980fe0f
Blookers <blookyl305@gmail.com>
2023-03-25 22:26:58 +1000

    fix build error again

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 //import adapter from 'sveltekit-adapter-chrome-extension';
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-vercel';
 import preprocess from 'svelte-preprocess';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
