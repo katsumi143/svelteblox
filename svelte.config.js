e2e26e0167d10944ebbb997d51c331a686e70d8e
Blookers <blookyl305@gmail.com>
2023-03-25 23:25:56 +1000

    squid 2

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,11 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 export default {
 	kit: {
-		adapter: adapter({
-			fallback: 'index.html'
-		})
+		adapter: adapter()
 	},
 	extensions: ['.svelte'],
 	preprocess: [
