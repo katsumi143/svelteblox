6e989099feb97a79aba343132b9da32de82f56d8
Blookers <blookyl305@gmail.com>
2023-05-13 14:23:18 +1000

    increase group icon cache time

--- src/lib/api/groups.ts
+++ src/lib/api/groups.ts
@@ -13,10 +13,10 @@ export function getGroup(id: string | number): Promise<Group> {
 	return GROUPS_CACHE.use(`group_${id}`, () => request<Group>(`${GROUPS_BASE}/groups/${id}`), 600000);
 }
 export function getGroupIcon(id: string | number): Promise<ImageData | undefined> {
-	return GROUPS_CACHE.use(`group_icon_${id}`, () => getGroupIcons([id]).then(data => data[0]), 600000);
+	return GROUPS_CACHE.use(`group_icon_${id}`, () => getGroupIcons([id]).then(data => data[0]), 3600000);
 }
 export function getGroupIcons(ids: (string | number)[]) {
-	return getThumbnails(ids, 'groups/icons?groupIds=%IDS%&format=Png&size=150x150');
+	return getThumbnails(ids, 'groups/icons?groupIds=%IDS%&format=Png&size=150x150', 3600000);
 }
 export function getGroupExperiences(id: string | number, access?: number) {
 	return request<ApiDataList<ExperienceV2>>(`https://games.roblox.com/v2/groups/${id}/games?accessFilter=${access}`)
