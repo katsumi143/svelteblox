a4d89bdf8067e90b7deec2a9ea7ed1682621fbdf
katsumi143 <32640219+katsumi143@users.noreply.github.com>
2023-07-29 00:35:11 +1000

    remove zwinplayer64 client channel

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,6 +1,6 @@
 export const THEMES = ['dark', 'light', 'color_purple'] as const;
 export const LOCALES = ['en-AU', 'lv-LV', 'ru-RU', 'ja-JP'] as const;
-export const CLIENT_CHANNELS = ['live', 'zcanary', 'zintegration', 'zwinplayer64'] as const;
+export const CLIENT_CHANNELS = ['live', 'zcanary', 'zintegration'] as const;
 
 export const LOCALE_MAP: Record<string, string> = {
 	en: 'en-AU',
